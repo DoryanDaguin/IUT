48d31404eb33e815f353da6cd3f53741b160fe81
DoryanDaguin <62285703+DoryanDaguin@users.noreply.github.com>
2020-03-26 12:23:52 +0100

    Update test_roulette.py

--- test_roulette.py
+++ test_roulette.py
@@ -5,6 +5,14 @@ On vérifie que la valeur est supérieur à 20
 >>> montant_argent > 20
 True
 
+On verifie que si on a le meme chiffre on gagne
+>>> gagne_ou_perdu(15,15)
+Bravo
+
+On verifie que si on a pas le meme chiffre on perd
+>>> gagne_ou_perdu(15,13)
+Perdu
+
 """
 
 def porte_monnaie_f_reussie(Nombre_joueurs):
@@ -14,6 +22,10 @@ def porte_monnaie_f_reussie(Nombre_joueurs):
     return argents
 montant_argent = porte_monnaie_f_reussie(2)
 
+def gagne_ou_perdu(joueurs_numero,numero_gagnant):
+    if joueurs_numero == numero_gagnant:
+        return print("Bravo")
+    return print("Perdu")
 
 if __name__ == '__main__':
 
