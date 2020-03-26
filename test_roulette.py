53628ef1b241449846daba0e1a84dce2fb8a300a
DoryanDaguin <doryan.daguin@gmail.com>
2020-03-26 12:56:37 +0000

    Test de code de la roulette

--- test_roulette.py
+++ test_roulette.py
@@ -1,17 +1,33 @@
 """
 
-On vérifie que la valeur est supérieur à 20
+Test 1 : On vérifie que la valeur est supérieur à 20
 
 >>> montant_argent > 20
 True
 
-On verifie que si on a le meme chiffre on gagne
+Test 2 : On verifie que si on a le meme chiffre on gagne
 >>> gagne_ou_perdu(15,15)
 Bravo
 
-On verifie que si on a pas le meme chiffre on perd
+Test 3 : On verifie que si on a pas le meme chiffre on perd
 >>> gagne_ou_perdu(15,13)
 Perdu
+
+Test 4 :On verifie que si on a la meme couleur on gagne
+>>> bonnecouleur_oupas(26,14)
+Bravo meme couleur
+
+Test 5 :On verifie que si on le numero mise est >36 il y a un message d'erreur
+>>> numero_mise(39)
+Nombre >36
+
+Test 6 :On verifie que si on on mise plus que l'argent qu'on a il y a un message d'erreur
+>>> argent_mise(150,100)
+Mise > argent possede
+
+Test 7 :On verifie que si on on mise 0 ou moins il y a un message d'erreur
+>>> argent_mise(-20,100)
+Mise negative
 
 """
 
@@ -27,6 +43,31 @@ def gagne_ou_perdu(joueurs_numero,numero_gagnant):
     if joueurs_numero == numero_gagnant:
         return print("Bravo")
     return print("Perdu")
+
+def bonnecouleur_oupas(joueurs_numero,numero_gagnant):
+    if joueurs_numero % 2 == numero_gagnant % 2:
+        return print("Bravo meme couleur")
+    return print("Perdu")
+
+def numero_mise(numero):
+  try:
+    numero = int(numero)
+  except ValueError:
+    print("Erreur")
+  if numero < 0:
+    print("Nombre négatif")
+  if numero > 36:
+    print("Nombre >36")
+
+def argent_mise(mise, porte_monnaie):
+  try:
+    mise = int(mise)
+  except ValueError:
+    print("Erreur")
+  if mise <= 0:
+    print("Mise negative")
+  if mise > porte_monnaie:
+    print("Mise > argent possede")
 
 if __name__ == '__main__':
 
